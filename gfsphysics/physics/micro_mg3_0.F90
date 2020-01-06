63ddb607a967758f7ea75b0d860541e58c1f6925
anning.cheng <anning.cheng@noaa.gov>
2020-01-06 16:07:42 -0500

    regression test for iccn=1 and iccn=2

--- gfsphysics/physics/micro_mg3_0.F90
+++ gfsphysics/physics/micro_mg3_0.F90
@@ -505,7 +505,7 @@ subroutine micro_mg_tend (                                       &
 !--ag
      freqs,                        freqr,                        &
      nfice,                        qcrat,                        &
-     prer_evap, xlat, xlon, lprnt, iccn, aero_in, nlball)
+     prer_evap, xlat, xlon, lprnt, iccn, nlball)
 
   ! Constituent properties.
   use micro_mg_utils, only: mg_liq_props,    &
@@ -593,7 +593,8 @@ subroutine micro_mg_tend (                                       &
   real(r8), intent(in) :: liqcldf(mgncol,nlev)    ! liquid cloud fraction (no units)
   real(r8), intent(in) :: icecldf(mgncol,nlev)    ! ice cloud fraction (no units)
   real(r8), intent(in) :: qsatfac(mgncol,nlev)    ! subgrid cloud water saturation scaling factor (no units)
-  logical, intent(in)  :: lprnt, iccn, aero_in
+  logical, intent(in)  :: lprnt
+  integer, intent(in)  :: iccn
 
 
   ! used for scavenging
@@ -1441,7 +1442,7 @@ subroutine micro_mg_tend (                                       &
     enddo
   enddo
 !
-  if (iccn) then
+  if (iccn == 1) then
     do k=1,nlev
       do i=1,mgncol
         npccn(i,k) = npccnin(i,k)
@@ -1495,7 +1496,7 @@ subroutine micro_mg_tend (                                       &
     enddo
   enddo
 
-  if (iccn) then
+  if (iccn == 1) then
     do k=1,nlev
       do i=1,mgncol
         if (t(i,k) < icenuct) then
@@ -1510,7 +1511,7 @@ subroutine micro_mg_tend (                                       &
         endif
       enddo
     enddo
-  elseif (aero_in) then
+  elseif (iccn == 2) then
     do k=1,nlev
       do i=1,mgncol
         if (t(i,k) < icenuct) then
