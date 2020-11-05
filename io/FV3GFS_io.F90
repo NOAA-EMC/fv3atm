5530a85cb22179178615db80e6304c31e02fd6ca
Dom Heinzeller <dom.heinzeller@icloud.com>
2020-11-05 11:21:51 -0700

    Add/update HWRF physics (#188)
    HWRF physics implementation in CCPP
    - addition of new CCPP schemes to ccpp_prebuild_config.py
    - update of suite definition files related to HWRF physics
    - update of GFS_typdefs.{F90,meta} with new/modified variables for HWRF physics
    - additional logic in FV3GFS_io.F90 for HWRF Noah LSM
    
    Co-authored-by: Jun.Wang <Jun.Wang@noaa.gov>
    Co-authored-by: Jili Dong <Jili.Dong@noaa.gov>
    Co-authored-by: Bin Liu <Bin.Liu@noaa.gov>
    Co-authored-by: Chunxi.Zhang-NOAA <Chunxi.Zhang@noaa.gov>
    Co-authored-by: andrew.hazelton <andrew.hazelton@noaa.gov>
    Co-authored-by: Jili.Dong@noaa.gov <Jili.Dong@v72a1.ncep.noaa.gov>
    Co-authored-by: ZhanZhang-NOAA <zhan.zhang@noaa.gov>
    Co-authored-by: Grant Firl <grantf@ucar.edu>
    Co-authored-by: Man.Zhang <Man.Zhang@noaa.gov>
    Co-authored-by: Dom Heinzeller <dom.heinzeller@noaa.gov>

--- io/FV3GFS_io.F90
+++ io/FV3GFS_io.F90
@@ -252,7 +252,7 @@ module FV3GFS_io_mod
        temp2d(i,j,33) = IPD_Data(nb)%Sfcprop%tprcp(ix)
        temp2d(i,j,34) = IPD_Data(nb)%Sfcprop%srflag(ix)
 #ifdef CCPP
-     if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp) then
+     if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4) then
 #endif
        temp2d(i,j,35) = IPD_Data(nb)%Sfcprop%slc(ix,1)
        temp2d(i,j,36) = IPD_Data(nb)%Sfcprop%slc(ix,2)
@@ -741,7 +741,7 @@ module FV3GFS_io_mod
       allocate(sfc_name3(0:nvar_s3+nvar_s3mp))
 
       allocate(sfc_var2(nx,ny,nvar_s2m+nvar_s2o+nvar_s2mp+nvar_s2r),sfc_var3ice(nx,ny,Model%kice))
-      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. (.not.warm_start)) then
+      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4 .or. (.not.warm_start)) then
         allocate(sfc_var3(nx,ny,Model%lsoil,nvar_s3))
       else if (Model%lsm == Model%lsm_ruc) then
         allocate(sfc_var3(nx,ny,Model%lsoil_lsm,nvar_s3))
@@ -920,7 +920,7 @@ module FV3GFS_io_mod
 
  
 #ifdef CCPP
-    if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. (.not.warm_start)) then
+    if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4 .or. (.not.warm_start)) then
       !--- names of the 3D variables to save
       sfc_name3(1) = 'stc'
       sfc_name3(2) = 'smc'
@@ -1185,7 +1185,7 @@ module FV3GFS_io_mod
         endif
 
 #ifdef CCPP
-        if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. (.not.warm_start)) then
+        if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4 .or. (.not.warm_start)) then
           !--- 3D variables
           do lsoil = 1,Model%lsoil
             Sfcprop(nb)%stc(ix,lsoil) = sfc_var3(i,j,lsoil,1)   !--- stc
@@ -1774,7 +1774,7 @@ module FV3GFS_io_mod
       allocate(sfc_name2(nvar2m+nvar2o+nvar2mp+nvar2r))
       allocate(sfc_name3(0:nvar3+nvar3mp))
       allocate(sfc_var2(nx,ny,nvar2m+nvar2o+nvar2mp+nvar2r))
-      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp) then
+      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4) then
         allocate(sfc_var3(nx,ny,Model%lsoil,nvar3))
       elseif (Model%lsm == Model%lsm_ruc) then
         allocate(sfc_var3(nx,ny,Model%lsoil_lsm,nvar3))
@@ -1943,7 +1943,7 @@ module FV3GFS_io_mod
       nullify(var2_p)
 
 #ifdef CCPP
-      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp) then
+      if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4) then
         !--- names of the 3D variables to save
         sfc_name3(1) = 'stc'
         sfc_name3(2) = 'smc'
@@ -2138,7 +2138,7 @@ module FV3GFS_io_mod
           sfc_var3ice(i,j,k) = Sfcprop(nb)%tiice(ix,k) !--- internal ice temperature
         end do
 
-        if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp) then
+        if (Model%lsm == Model%lsm_noah .or. Model%lsm == Model%lsm_noahmp .or. Model%lsm == Model%lsm_noah_wrfv4) then
           !--- 3D variables
           do lsoil = 1,Model%lsoil
             sfc_var3(i,j,lsoil,1) = Sfcprop(nb)%stc(ix,lsoil) !--- stc
