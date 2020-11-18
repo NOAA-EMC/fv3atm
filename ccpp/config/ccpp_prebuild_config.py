d32af817fc279fb9e4f3df19f0f07fc2d52c0f01
Dustin Swales <dustin.swales@noaa.gov>
2020-11-18 21:31:07 +0000

    RRTMGP working with Thompson MP.

--- ccpp/config/ccpp_prebuild_config.py
+++ ccpp/config/ccpp_prebuild_config.py
@@ -201,6 +201,7 @@ SCHEME_FILES = [
     'ccpp/physics/physics/GFS_rrtmgp_thompsonmp_pre.F90',
     'ccpp/physics/physics/GFS_rrtmgp_gfdlmp_pre.F90',
     'ccpp/physics/physics/GFS_rrtmgp_zhaocarr_pre.F90',
+    'ccpp/physics/physics/GFS_rrtmgp_cloud_overlap_pre.F90',
     'ccpp/physics/physics/GFS_rrtmgp_sw_post.F90'
     ]
 
