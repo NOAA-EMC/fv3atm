5ab450b59a1ecfce426df7459580f462b98de5a5
XiaSun-NOAA <xia.sun@noaa.gov>
2021-04-27 18:25:30 +0000

    add ncld consistency check in GFS_typedefs.F90

--- ccpp/data/GFS_typedefs.F90
+++ ccpp/data/GFS_typedefs.F90
@@ -4622,6 +4622,12 @@ module GFS_typedefs
       Model%nqvdelt  = 4
       Model%nps2delt = 1
       Model%npsdelt  = 2
+      
+      if (Model%ncld /= 1) then
+        print *,' Zhao-Carr MP requires ncld to be set to 1 - job aborted'
+        stop
+      end if
+
       if (Model%me == Model%master) print *,' Using Zhao/Carr/Sundqvist Microphysics'
 
     elseif (Model%imp_physics == Model%imp_physics_zhao_carr_pdf) then !Zhao Microphysics with PDF cloud
@@ -4645,6 +4651,10 @@ module GFS_typedefs
       Model%nleffr  = 1
       Model%nieffr  = 2
       Model%nseffr  = 3
+      if (Model%ncld /= 5) then
+        print *,' Ferrier-Aligo MP requires ncld to be set to 5 - job aborted'
+        stop
+      end if
       if (Model%me == Model%master) print *,' Using Ferrier-Aligo MP scheme', &
                                           ' microphysics', &
                                           ' lradar =',Model%lradar
@@ -4672,6 +4682,11 @@ module GFS_typedefs
       Model%nleffr  = 1
       Model%nieffr  = 2
       Model%nseffr  = 3
+
+      if (Model%ncld /= 5) then
+        print *,' Thompson MP requires ncld to be set to 5 - job aborted'
+        stop
+      end if         
       if (.not. Model%effr_in) then
         print *,' Thompson MP requires effr_in to be set to .true. - job aborted'
         stop
@@ -4696,6 +4711,10 @@ module GFS_typedefs
       Model%nieffr  = 3
       Model%nreffr  = 4
       Model%nseffr  = 5
+      if (Model%ncld /= 2) then
+        print *,' Morrison-Gettelman MP requires ncld to be set to 2 - job aborted'
+        stop
+      end if
       if (abs(Model%fprcp) == 1) then
         Model%ncnd  = 4
       elseif (Model%fprcp >= 2) then
@@ -4743,6 +4762,10 @@ module GFS_typedefs
       Model%pdfcld  = .false.
       Model%shcnvcw = .false.
       Model%ncnd    = 5
+      if (Model%ncld /= 5) then
+        print *,' GFDL MP requires ncld to be set to 5 - job aborted'
+        stop
+      end if
       if (Model%me == Model%master) print *,' avg_max_length=',Model%avg_max_length
       if (Model%me == Model%master) print *,' Using GFDL Cloud Microphysics'
     else
