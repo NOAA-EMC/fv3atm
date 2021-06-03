76d546b885160c0cd9d32ce68fc6ecc771f76d41
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2021-06-03 03:32:26 +0000

    Revert "Rename temporarily upward sensible heat flux field"
    This reverts commit 8a98b80f60d3520e1c288d2476527787650e909e.

--- atmos_model.F90
+++ atmos_model.F90
@@ -1230,8 +1230,7 @@ subroutine update_atmos_chemistry(state, rc)
       if (ESMF_LogFoundError(rcToCheck=localrc, msg=ESMF_LOGERR_PASSTHRU, &
         line=__LINE__, file=__FILE__, rcToReturn=rc)) return
 
-!     call cplFieldGet(state,'inst_up_sensi_heat_flx', farrayPtr2d=shfsfc, rc=localrc)
-      call cplFieldGet(state,'inst_sensi_heat_flx', farrayPtr2d=shfsfc, rc=localrc)
+      call cplFieldGet(state,'inst_up_sensi_heat_flx', farrayPtr2d=shfsfc, rc=localrc)
       if (ESMF_LogFoundError(rcToCheck=localrc, msg=ESMF_LOGERR_PASSTHRU, &
         line=__LINE__, file=__FILE__, rcToReturn=rc)) return
 
