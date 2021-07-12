20883edcb5f80cbcf1ea89f721d3c58a837cc760
Dom Heinzeller <dom.heinzeller@icloud.com>
2021-07-12 07:06:46 -0600

    Add extended diagnostic output from Thompson MP, includes "Add optional scaling to RRTMGP flux adjustment" (#319), bug fix in several suite definition files (#331)
    Adds a set of dedicated 3d arrays for extended diagnostic output from Thompon MP, and a logical flag that controls allocating, outputting and resetting these arrays. This work is based on @ericaligo-NOAA 's code changes.
    
    The flag to reset the extended diagnostics from Thompson MP is - for the UFS - currently tied to avg_max_length, which is used to reset maximum hourly fields (the corresponding reset variable is renamed to make its purpose clear).
    
    Include the changes in #319, "Add optional scaling to RRTMGP flux adjustment".
    
    Important: Fixed bugs in several suite definition files that were missing the call to GFS_radiation_surface:
    
    * suite_FV3_GFS_v15_thompson_mynn_RRTMGP.xml
    * suite_FV3_GFS_v16_coupled_noahmp.xml
    * suite_FV3_GFS_v16_coupled_nsstNoahmp.xml
    * suite_FV3_RRFS_v1alpha.xml
    
    Co-authored-by: Dustin Swales <dustin.swales@noaa.gov>
    Co-authored-by: Eric Aligo <eric.aligo@noaa.gov>
    Co-authored-by: Dom Heinzeller <dom.heinzeller@noaa.gov>

--- ccpp/driver/GFS_diagnostics.F90
+++ ccpp/driver/GFS_diagnostics.F90
@@ -3676,6 +3676,32 @@ module GFS_diagnostics
       enddo
     endif
 
+    ! Extended diagnostics from Thompson MP
+    thompson_extended_diagnostics: if (Model%ext_diag_thompson) then
+      do num=1,Model%thompson_ext_ndiag3d
+        idx = idx + 1
+        ExtDiag(idx)%axes = 3
+        select case (num)
+          ! This is the place to add specific names, descriptions,
+          ! and units if so desired
+          !case (1)
+          ! ...
+          case default
+            write (xtra,'(I2.2)') num
+            ExtDiag(idx)%name = 'thompson_diag3d_' // trim(xtra)
+            ExtDiag(idx)%desc = 'Thompson extended diagnostics array ' // trim(xtra)
+            ExtDiag(idx)%unit = 'unknown'
+        end select
+        ExtDiag(idx)%mod_name = 'gfs_phys'
+        ExtDiag(idx)%intpl_method = 'bilinear'
+        ExtDiag(idx)%time_avg = .false.
+        allocate (ExtDiag(idx)%data(nblks))
+        do nb = 1,nblks
+          ExtDiag(idx)%data(nb)%var3 => IntDiag(nb)%thompson_ext_diag3d(:,:,num)
+        enddo
+      enddo
+    end if thompson_extended_diagnostics
+
     !! Cloud effective radii from Microphysics
     !if (Model%imp_physics == Model%imp_physics_thompson .or. Model%imp_physics == Model%imp_physics_wsm6 .or. Model%imp_physics == Model%imp_physics_fer_hires) then
     !  idx = idx + 1
