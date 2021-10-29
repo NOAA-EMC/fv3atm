c48588155f98ddf4cd2766337d55da6e60e3c373
Ted Mansell <ted.mansell@noaa.gov>
2021-10-29 16:50:03 +0000

    Made setting of otsptflag more logical

--- ccpp/data/GFS_typedefs.F90
+++ ccpp/data/GFS_typedefs.F90
@@ -7612,6 +7612,7 @@ module GFS_typedefs
     class(GFS_interstitial_type)       :: Interstitial
     type(GFS_control_type), intent(in) :: Model
     integer :: n, tracers
+    logical :: ltest
 
     !first, initialize the values (in case the values don't get initialized within if statements below)
     Interstitial%nvdiff           = Model%ntrac
@@ -7741,11 +7742,13 @@ module GFS_typedefs
       Interstitial%otsptflag(:) = .true.
       tracers = 2
       do n=2,Model%ntrac
-        if ( n /= Model%ntcw  .and. n /= Model%ntiw  .and. n /= Model%ntclamt .and. &
+        ltest = ( n /= Model%ntcw  .and. n /= Model%ntiw  .and. n /= Model%ntclamt .and. &
              n /= Model%ntrw  .and. n /= Model%ntsw  .and. n /= Model%ntrnc   .and. &
              n /= Model%ntsnc .and. n /= Model%ntgl  .and. n /= Model%ntgnc   .and. &
              n /= Model%nthl  .and. n /= Model%nthnc .and. n /= Model%ntgv    .and. &
-             n /= Model%nthv  .and. n /= Model%ntccn .and. n /= Model%ntccna ) then
+             n /= Model%nthv  .and. n /= Model%ntccn .and. n /= Model%ntccna )
+        Interstitial%otsptflag(n) = ltest
+        if ( ltest ) then
           tracers = tracers + 1
           if (Model%ntke  == n ) then
             Interstitial%otspt(tracers+1,1) = .false.
@@ -7756,8 +7759,6 @@ module GFS_typedefs
 !               ntrw  == n .or. ntsw  == n .or. ntgl  == n)                    &
                   Interstitial%otspt(tracers+1,1) = .false.
           if (Interstitial%trans_aero .and. Model%ntchs == n) Interstitial%itc = tracers
-        else
-          Interstitial%otsptflag(n) = .false.
         endif
       enddo
       Interstitial%tracers_total = tracers - 2
