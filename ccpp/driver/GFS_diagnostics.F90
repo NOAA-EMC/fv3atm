543efe115c7f0e875a157153b75d18ba2fe9fe99
JeffBeck-NOAA <55201531+JeffBeck-NOAA@users.noreply.github.com>
2021-12-27 21:58:01 -0700

    Update ccpp/driver/GFS_diagnostics.F90
    Co-authored-by: Dom Heinzeller <dom.heinzeller@icloud.com>

--- ccpp/driver/GFS_diagnostics.F90
+++ ccpp/driver/GFS_diagnostics.F90
@@ -2331,7 +2331,7 @@ module GFS_diagnostics
       ExtDiag(idx)%mod_name = 'gfs_phys'
       allocate (ExtDiag(idx)%data(nblks))
       do nb = 1,nblks
-        ExtDiag(idx)%data(nb)%var3 => IntDiag(nb)%spp_wts_mp(:,:)
+        ExtDiag(idx)%data(nb)%var3 => Coupling(nb)%spp_wts_mp(:,:)
       enddo
     endif
     
