c8b8da7e228e010b17baccfef444fd2071cc545b
Dustin Swales <dustin.swales@noaa.gov>
2022-08-26 15:29:55 +0000

    Changes to typedefs for RRTMGP refactoring. Updates to prebuild and SDFs.

--- ccpp/config/ccpp_prebuild_config.py
+++ ccpp/config/ccpp_prebuild_config.py
@@ -22,11 +22,6 @@ VARIABLE_DEFINITION_FILES = [
     'physics/physics/h2o_def.f',
     'physics/physics/ozne_def.f',
     'physics/physics/radiation_surface.f',
-    'physics/physics/rte-rrtmgp/rrtmgp/mo_gas_optics_rrtmgp.F90',
-    'physics/physics/rte-rrtmgp/rrtmgp/mo_gas_concentrations.F90',
-    'physics/physics/rte-rrtmgp/rte/mo_optical_props.F90',
-    'physics/physics/rte-rrtmgp/extensions/cloud_optics/mo_cloud_optics.F90',
-    'physics/physics/rte-rrtmgp/rte/mo_source_functions.F90',
     'data/CCPP_typedefs.F90',
     'data/GFS_typedefs.F90',
     'data/CCPP_data.F90',
@@ -67,27 +62,6 @@ TYPEDEFS_NEW_METADATA = {
         'GFS_grid_type'         : 'GFS_Data(cdata%blk_no)%Grid',
         'GFS_stateout_type'     : 'GFS_Data(cdata%blk_no)%Stateout',
         'GFS_typedefs' : '',
-        },
-    'mo_gas_concentrations' : {
-        'ty_gas_concs' : '',
-        'mo_gas_concentrations' : '',
-        },
-    'mo_gas_optics_rrtmgp' : {
-        'ty_gas_optics_rrtmgp' : '',
-        'mo_gas_optics_rrtmgp' : '',
-        },
-    'mo_optical_props' : {
-        'ty_optical_props_1scl' : '',
-        'ty_optical_props_2str' : '',
-        'mo_optical_props' : '',
-        },
-    'mo_cloud_optics' : {
-        'ty_cloud_optics' : '',
-        'mo_cloud_optics' : '',
-        },
-    'mo_source_functions' : {
-        'ty_source_func_lw' : '',
-        'mo_source_functions' : '',
         },
     }
 
@@ -218,23 +192,15 @@ SCHEME_FILES = [
     'physics/smoke/rrfs_smoke_postpbl.F90',
     'physics/smoke/rrfs_smoke_lsdep_wrapper.F90',
     # RRTMGP
-    'physics/physics/rrtmgp_lw_gas_optics.F90',
-    'physics/physics/rrtmgp_lw_cloud_optics.F90',
-    'physics/physics/rrtmgp_sw_gas_optics.F90',
-    'physics/physics/rrtmgp_sw_cloud_optics.F90',
     'physics/physics/rrtmgp_aerosol_optics.F90',
-    'physics/physics/rrtmgp_lw_rte.F90',
-    'physics/physics/rrtmgp_sw_rte.F90',
+    'physics/physics/rrtmgp_lw_main.F90',
+    'physics/physics/rrtmgp_sw_main.F90',
     'physics/physics/GFS_rrtmgp_setup.F90',
     'physics/physics/GFS_rrtmgp_pre.F90',
-    'physics/physics/rrtmgp_lw_pre.F90',
-    'physics/physics/GFS_rrtmgp_lw_post.F90',
-    'physics/physics/rrtmgp_lw_cloud_sampling.F90',
-    'physics/physics/rrtmgp_sw_cloud_sampling.F90',
     'physics/physics/GFS_cloud_diagnostics.F90',
     'physics/physics/GFS_rrtmgp_cloud_mp.F90',
     'physics/physics/GFS_rrtmgp_cloud_overlap.F90',
-    'physics/physics/GFS_rrtmgp_sw_post.F90'
+    'physics/physics/GFS_rrtmgp_post.F90'
     ]
 
 # Default build dir, relative to current working directory,
--- ccpp/data/GFS_typedefs.F90
+++ ccpp/data/GFS_typedefs.F90
@@ -785,6 +785,7 @@ module GFS_typedefs
     logical              :: doGP_lwscat             !< If true, include scattering in longwave cloud-optics, only compatible w/ GP cloud-optics
     logical              :: doGP_sgs_cnv            !< If true, include SubGridScale convective cloud in RRTMGP
     logical              :: doGP_sgs_mynn           !< If true, include SubGridScale MYNN-EDMF cloud in RRTMGP 
+    integer              :: rrtmgp_phys_blksz       !< Number of columns to pass to RRTMGP per block.
     real(kind_phys)      :: minGPpres               !< Minimum pressure allowed in RRTMGP.
     real(kind_phys)      :: maxGPpres               !< Maximum pressure allowed in RRTMGP.
     real(kind_phys)      :: minGPtemp               !< Minimum temperature allowed in RRTMGP.
@@ -3010,6 +3011,7 @@ module GFS_typedefs
     logical              :: doGP_lwscat         = .false.    !< If true, include scattering in longwave cloud-optics, only compatible w/ GP cloud-optics
     logical              :: doGP_sgs_cnv        = .false.    !< If true, include SubGridScale convective cloud in RRTMGP
     logical              :: doGP_sgs_mynn       = .false.    !< If true, include SubGridScale MYNN-EDMF cloud in RRTMGP
+    integer              :: rrtmgp_phys_blksz   = 1          !< Number of columns for RRTMGP to process at each instance.
 !--- Z-C microphysical parameters
     integer              :: imp_physics       =  99                !< choice of cloud scheme
     real(kind=kind_phys) :: psautco(2)        = (/6.0d-4,3.0d-4/)  !< [in] auto conversion coeff from ice to snow
@@ -3468,6 +3470,7 @@ module GFS_typedefs
                                rrtmgp_nrghice, rrtmgp_nGauss_ang, do_GPsw_Glw,              &
                                use_LW_jacobian, doGP_lwscat, damp_LW_fluxadj, lfnc_k,       &
                                lfnc_p0, iovr_convcld, doGP_sgs_cnv, doGP_sgs_mynn,          &
+                               rrtmgp_phys_blksz,                                           &
                           ! IN CCN forcing
                                iccn,                                                        &
                           !--- microphysical parameterizations
@@ -3910,6 +3913,7 @@ module GFS_typedefs
     Model%doGP_lwscat         = doGP_lwscat
     Model%doGP_sgs_cnv        = doGP_sgs_cnv
     Model%doGP_sgs_mynn       = doGP_sgs_mynn
+    Model%rrtmgp_phys_blksz   = rrtmgp_phys_blksz
     if (Model%do_RRTMGP) then
        ! RRTMGP incompatible with levr /= levs
        if (Model%levr /= Model%levs) then
@@ -5778,6 +5782,7 @@ module GFS_typedefs
         print *, ' doGP_sgs_cnv       : ', Model%doGP_sgs_cnv
         print *, ' doGP_sgs_mynn      : ', Model%doGP_sgs_cnv
         print *, ' iovr_convcld       : ', Model%iovr_convcld
+        print *, ' rrtmgp_phys_blksz  : ', Model%rrtmgp_phys_blksz
       endif
       print *, ' '
       print *, 'microphysical switch'
