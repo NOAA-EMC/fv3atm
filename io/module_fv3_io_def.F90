de354355e71674cb2e87469f5115ad25f50b86b3
Edward Hartnett <edward.hartnett@noaa.gov>
2024-01-16 05:15:14 -0700

    doxygen changes

--- io/module_fv3_io_def.F90
+++ io/module_fv3_io_def.F90
@@ -1,36 +1,126 @@
+!> @file
+!> @brief fv3 I/O related configration variables.
+!> @author Jun Wang @date 01/2017
+
+!> fv3 I/O related configration variables.
+!>
+!> @author Jun Wang @date 01/2017
 module module_fv3_io_def
-!
-!*** fv3 io related configration variables
-!
-! revision history
-! 01/2017   Jun Wang    Initial code
-!
-!------------------------------------------------------------------------
-!
   use esmf, only     : esmf_maxstr
   implicit none
 
+  !> Number of processors used in forecast run.
   integer           :: num_pes_fcst
-  integer           :: wrttasks_per_group, write_groups
-  integer           :: n_group
-  integer           :: num_files
-  integer           :: nbdlphys
-  integer           :: iau_offset
-  logical           :: lflname_fulltime
-  logical           :: time_unlimited
 
-  character(len=esmf_maxstr),dimension(:),allocatable :: filename_base
-  character(len=esmf_maxstr),dimension(:),allocatable :: output_file
+  !> Number of write tasks per group.  
+  integer           :: wrttasks_per_group
 
-  integer,dimension(:),allocatable     :: lead_wrttask, last_wrttask
+  !> ???  
+  integer           :: write_groups 
 
-  character(len=esmf_maxstr),dimension(:),allocatable :: output_grid
-  integer,dimension(:),allocatable  :: imo,jmo
-  real,dimension(:),allocatable     :: cen_lon, cen_lat
-  real,dimension(:),allocatable     :: lon1, lat1, lon2, lat2, dlon, dlat
-  real,dimension(:),allocatable     :: stdlat1, stdlat2, dx, dy
-  integer,dimension(:),allocatable  :: ideflate, quantize_nsd, zstandard_level
+  !> ???  
+  integer           :: n_group 
+
+  !> ???  
+  integer           :: num_files 
+
+  !> ???  
+  integer           :: nbdlphys 
+
+  !> ???  
+  integer           :: iau_offset 
+
+  !> ???  
+  logical           :: lflname_fulltime 
+
+  !> ???  
+  logical           :: time_unlimited 
+
+
+  !> ???  
+  character(len=esmf_maxstr),dimension(:),allocatable :: filename_base  
+
+  !> ???  
+  character(len=esmf_maxstr),dimension(:),allocatable :: output_file 
+
+
+  !> ???  
+  integer,dimension(:),allocatable     :: lead_wrttask 
+
+  !> ???  
+  integer,dimension(:),allocatable     :: last_wrttask 
+
+
+  !> ???  
+  character(len=esmf_maxstr),dimension(:),allocatable :: output_grid 
+
+  !> ???  
+  integer,dimension(:),allocatable  :: imo 
+
+  !> ???  
+  integer,dimension(:),allocatable  :: jmo 
+
+  !> ???  
+  real,dimension(:),allocatable     :: cen_lon 
+
+  !> ???  
+  real,dimension(:),allocatable     :: cen_lat 
+
+  !> ???  
+  real,dimension(:),allocatable     :: lon1 
+
+  !> ???  
+  real,dimension(:),allocatable     :: lat1 
+
+  !> ???  
+  real,dimension(:),allocatable     :: lon2 
+
+  !> ???  
+  real,dimension(:),allocatable     :: lat2 
+
+  !> ???  
+  real,dimension(:),allocatable     :: dlon 
+
+  !> ???  
+  real,dimension(:),allocatable     :: dlat 
+
+  !> ???  
+  real,dimension(:),allocatable     :: stdlat1 
+
+  !> ???  
+  real,dimension(:),allocatable     :: stdlat2 
+
+  !> ???  
+  real,dimension(:),allocatable     :: dx 
+
+  !> ???  
+  real,dimension(:),allocatable     :: dy 
+
+  !> Deflate level to use, 0 means no deflate.
+  integer,dimension(:),allocatable  :: ideflate
+
+  !> Number of significant digits for lossy compression.
+  integer,dimension(:),allocatable  :: quantize_nsd
+
+  !> Zstandard compression level, 0 means no zstandard compression.
+  integer,dimension(:),allocatable  :: zstandard_level
+
+  !> Quantize mode to use for lossy compression.
   character(len=esmf_maxstr),dimension(:),allocatable :: quantize_mode
-  integer,dimension(:),allocatable  :: ichunk2d, jchunk2d, ichunk3d, jchunk3d, kchunk3d
+
+  !> Chunk size in i dimension for 2D data.
+  integer,dimension(:),allocatable  :: ichunk2d
+
+  !> Chunk size in j dimension for 2D data.
+  integer,dimension(:),allocatable  :: jchunk2d
+
+  !> Chunk size in i dimension for 3D data.
+  integer,dimension(:),allocatable  :: ichunk3d
+
+  !> Chunk size in j dimension for 3D data.
+  integer,dimension(:),allocatable  :: jchunk3d
+
+  !> Chunk size in k dimension for 3D data.
+  integer,dimension(:),allocatable  :: kchunk3d
 
 end module module_fv3_io_def
--- module_fv3_config.F90
+++ module_fv3_config.F90
@@ -1,28 +1,49 @@
+!> @file
+!> @brief fv3 configure variables from model_configure.
+!> @author Jun Wang @date 01/2017
 
+!> fv3 configure variables from model_configure.
+!>
+!> @author Jun Wang @date 01/2017
   module module_fv3_config
-!------------------------------------------------------------------------
-!
-!*** fv3 configure variables from model_configure
-!
-! revision history
-! 01/2017   Jun Wang    Initial code
-!
-!------------------------------------------------------------------------
-!
   use esmf
 
   implicit none
-!
+
+
+  !> ???  
   integer                  :: dt_atmos
+
+  !> ???  
   integer                  :: first_kdt
-  integer                  :: fcst_mpi_comm, fcst_ntasks
-!
+
+  !> ???  
+  integer                  :: fcst_mpi_comm
+
+  !> ???  
+  integer                  :: fcst_ntasks
+
+
+  !> ???  
   integer                  :: cpl_grid_id
+
+  !> ???  
   logical                  :: cplprint_flag
-  logical                  :: quilting, quilting_restart
-!
+
+  !> ???  
+  logical                  :: quilting
+
+  !> ???  
+  logical                  :: quilting_restart
+
+
+  !> ???  
   real,dimension(:),allocatable                   :: output_fh
+
+  !> ???  
   character(esmf_maxstr),dimension(:),allocatable :: filename_base
+
+  !> ???  
   character(17)            :: calendar='                 '
-!
+
   end module module_fv3_config
