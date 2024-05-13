b329c2ccc4c5c6936cb1f548eef83d2a1ee36ae2
Xiaqiong.Zhou <Xiaqiong.Zhou@noaa.gov>
2024-05-13 14:07:32 -0500

    revise conditions to calculate zint

--- io/post_fv3.F90
+++ io/post_fv3.F90
@@ -4345,9 +4345,10 @@ module post_fv3
         do j=jsta,jend
           do i=ista,iend
             if(wh(i,j,l) /= spval) then
-              if (omga(i,j,l) == spval .and. dpres(i,j,l) /= spval .and. zint(i,j,l) /=spval)  &
+              if(omga(i,j,l) == spval .and. dpres(i,j,l) /= spval .and. zint(i,j,l) /=spval)  &
                   omga(i,j,l) = (-1.) * wh(i,j,l) * dpres(i,j,l)/zint(i,j,l)
-              zint(i,j,l) = zint(i,j,l) + zint(i,j,l+1)
+              if(zint(i,j,l+1) /=spval .and. zint(i,j,l) /=spval) &
+                  zint(i,j,l) = zint(i,j,l) + zint(i,j,l+1)
             else
               if(zint(i,j,l+1) /=spval .and. t(i,j,l) /= spval .and.  alpint(i,j,l+1) /= spval  &
                              .and. alpint(i,j,l) /=spval .and. q(i,j,l) /= spval) then
